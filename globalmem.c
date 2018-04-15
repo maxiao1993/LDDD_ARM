fb24091c0a72cfea78eb76e73df908c1a6e5eb5b
maxiao1993 <soundwave_max@163.com>
2018-04-15 21:45:16 +0800

    update globalmem.c

--- globalmem.c
+++ globalmem.c
@@ -52,4 +52,96 @@ default:
 return 0;
 }
 
+static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	unsigned int count = size;
+	int ret = 0;
+	struct globalmem_dev *dev = filp->private_data;
 
+	if (p >= GLOBALMEM_SIZE)
+		return 0;
+	if (count > GLOBALMEM_SIZE - p)		//count is too large.
+		count = GLOBALMEM_SIZE - p;
+
+	if (copy_to_user(buf, dev->mem + p, count)) {
+		ret = -EFAULT;
+	} else {
+		*ppos += count;
+		ret = count;
+
+		printk(KERN_INFO "read %u bytes(s) from %lu\n", count, p);
+	}
+
+return ret;
+}
+
+static ssize_t globalmem_write (struct file *filp, const char __user *buf, size_t size, loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	unsigned int count = size;
+	int ret = 0;
+	struct globalmem_dev *dev = filp->private_data;
+
+	if (p >= GLOBALMEM_SIZE)
+		return 0;
+	if (count > GLOBALMEM_SIZE - p)
+		count = GLOBALMEM_SIZE - p;
+
+	if (copy_from_user(dev->mem + p, buf, count))
+		ret = -EFAULT;
+	else {
+		*ppos += count;
+		ret = count;
+
+		printk(KERN_INFO "write %u bytes(s) from %lu\n", count, p);
+	}
+
+	return ret;
+}
+
+static loff_t globalmem_llseek (struct file *filp, loff_t offset, int orig)
+{
+	loff_t ret = 0;
+	switch (orig) {
+	case 0:
+		if (offset < 0){
+			ret = -EINVAL;
+			break;
+		}
+		if ((unsigned int)offset > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		filp->f_pos = (unsigned int)offset;
+		ret = filp->f_pos;
+		break;
+	case 1:
+		if ((filp->f_pos + offset) > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		if ((filp->f_pos + offset) < 0) {
+			ret = - EINVAL;
+			break;
+		}
+		filp->f_pos += offset;
+		ret = filp->f_pos;
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+
+static const struct file_operations globalmem_fops = {
+	.owner = THIS_MODULE,
+	.llseek = globalmem_llseek,
+	.read = globalmem_read,
+	.write = globalmem_write,
+	.unlocked_ioctl = globalmem_ioctl,
+	.open = globalmem_open,
+	.release = globalmem_release,
+};

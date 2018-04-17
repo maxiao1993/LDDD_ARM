41eb3674da995b867f0510c5363ac29852f6e981
maxiao1993 <soundwave_max@163.com>
2018-04-17 10:39:38 +0800

    modified globalmem.c

--- globalmem.c
+++ globalmem.c
@@ -145,3 +145,54 @@ static const struct file_operations globalmem_fops = {
 	.open = globalmem_open,
 	.release = globalmem_release,
 };
+
+static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
+{
+	int err, devno = MKDEV(globalmem_major, index);
+
+	cdev_init(&dev->cdev, &globalmem_fops);
+	dev->cdev.owner = THIS_MODULE;
+	err = cdev_add(&dev->cdev, devno, 1);
+	if (err)
+		printk(KERN_NOTICE "Error %d adding globalmem%d", err, index);
+}
+
+static int __init globalmem_init(void)
+{
+	int ret;
+	dev_t devno = MKDEV(globalmem_major, 0);
+
+	if (globalmem_major)
+		ret = register_chrdev_region(devno, 1, "globalmem");
+	else {
+		ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+		globalmem_major = MAJOR(devno);
+	}
+	if (ret < 0)
+		return ret;
+
+	globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+	if (!globalmem_devp) {
+		ret = -ENOMEM;
+		goto fail_malloc;
+	}
+
+	globalmem_setup_cdev(globalmem_devp, 0);
+	return 0;
+
+	fail_malloc:
+	unregister_chrdev_region(devno, 1);
+	return ret;
+}
+module_init(globalmem_init);
+
+static void __exit globalmem_exit(void) 
+{
+	cdev_del(&globalmem_devp->cdev);
+	kfree(globalmem_devp);
+	unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
+}
+module_exit(globalmem_exit);
+
+MODULE_AUTHOR("Maxiao <soundwave_max@163.com>");
+MODULE_LICENSE("GPL v2");

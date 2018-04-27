9ae1e0919d9c4e8003aff475203739e1abf9809c
maxiao1993 <soundwave_max@163.com>
2018-04-27 10:55:39 +0800

    add second_test.c

---
+++ second/second_test.c
@@ -0,0 +1,23 @@
+#include<fcntl.h>
+#include<unistd.h>
+
+main()
+{
+	int fd;
+	int counter = 0;
+	int old_counter = 0;
+
+	/* 打开 /dev/second设备文件*/
+	fd = open("/dev/second", O_RDONLY);
+	if (fd != -1){
+		while (1){
+			read(fd, &counter, sizeof(unsigned int));	/*读目前经历的秒数*/
+			if (counter != old_counter){
+				printf("seconds after open /dev/second :%d\n", counter);
+				old_counter = counter;
+			}
+		}
+	} else {
+		printf("Device open failure\n");
+	}
+}
